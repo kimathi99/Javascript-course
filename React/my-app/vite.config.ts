7077b4ba4c87277e782b918945556a4884628364
kimathi99 <michaelkimathi81@gmail.com>
2025-02-22 17:01:09 +0000

    changes

--- React/my-app/vite.config.ts
+++ React/my-app/vite.config.ts
@@ -3,5 +3,17 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    
+  ],
+  server:{
+    cors:true,
+    host:true,
+    // allowedHosts: ['pug-bold-forcibly.ngrok-free.app'], // Add your allowed host here
+  },
+  build: {
+    outDir: 'dist',
+  }
+  
+  
 })
