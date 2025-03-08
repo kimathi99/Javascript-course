1e84bac644658654001a59f509dfe94a9d863cc3
kimathi99 <michaelkimathi81@gmail.com>
2025-03-08 17:04:09 +0000

    changes

--- To-do-app/todo-app/src/pages/homepage.tsx
+++ To-do-app/todo-app/src/pages/homepage.tsx
@@ -20,42 +20,49 @@ export const Homepage = function () {
   const [dueDate, setDueDate] = useState("");
   // managing the   date
 
-  const [editIndex, setEditIndex] = useState<number | null>(null);
+
+  const [editIndex, setEditIndex] = useState<number | null>(null); //store  the index ready for deletion
   const [showModal, setShowModal] = useState(false);
+  // a modal is a pop up  and it takes  two values either its shown or not  --boolean  value
   const [taskToDelete, setTaskToDelete] = useState<number | null>(null);
 
   const addingTask=()=>{
-    const newTask:Activity={
-      id:Date.now(),
-      name:task,
-      dueDate:dueDate,
-    }
-    setTodos([...todos,newTask]);
-    setTask("");
-    setDueDate("");
-
-  }
-  const addOrUpdateTask = () => {
-    if (editIndex !== null) {
+    if(editIndex!==null){
       const updatedTodos = todos.map((todo, index) =>
-        index === editIndex ? { ...todo, name: task, dueDate } : todo
+        index === editIndex ? { ...todo, name: task, dueDate } : todo//inital  value
       );
+      
       setTodos(updatedTodos);
       setEditIndex(null);
-    } else {
-      const newTask: Activity = {
-        id: Date.now(),
-        name: task,
-        dueDate,
-      };
-      setTodos([...todos, newTask]);
+
+    }else{
+      const newTask:Activity={
+        id:Date.now(),
+        name:task,//firstinput
+        dueDate:dueDate,//secondinput
+      }
+      setTodos([...todos,newTask]);
     }
+    
     setTask("");
     setDueDate("");
-  };
 
+    console.log(todos);
+
+  }
+  
   const handleEdit = (index: number) => {
     setEditIndex(index);
+    // 1. setting index for deletion
+    // 2.my inputs  are stored  in todo with a bluprint of activity --id --name --duedate
+    // 3.I filter out 
+    // michael=(a,b,c,d) .... to get value c ...kim[2]--I will get a single  objects---c
+    // todos
+    //  {/* {
+    //    0 activity one ---name--id--dueDate--react
+    //     1  activity two----name---id--duedate
+    //     2 activity two----name---id--duedate
+    // //   } */}  todos[index]....get a single  todo...gottentodo.name gottterntodo.id
     setTask(todos[index].name);
     setDueDate(todos[index].dueDate);
   };
@@ -68,9 +75,19 @@ export const Homepage = function () {
   const confirmDelete = () => {
     if (taskToDelete !== null) {
       const updatedTodos = todos.filter((_, index) => index !== taskToDelete);
+      // remember  we are using immutable state
+      // we cannot  change a  value directly  --we can only replace the value with  the new value 
+      // benefits  of immutable state --speed complixty  and storage complexity
+      // memory addresses  a ..b ..c  ..d 
+      // if I was  to do some operations it means  I have to acclocate some space
+      // a + b=c --
+      // value of a  stored  in a different address in the memory ---abx
+      // value of b  stored  in a different address in the memory ----aby
+      // value of c  stored  in a different address in the memory----abz--results
+      // delete and  moving --- but not changng the  memory address
       setTodos(updatedTodos);
-      setTaskToDelete(null);
-      setShowModal(false);
+      setTaskToDelete(null);//---state  that was holding the  index for deletion
+      setShowModal(false);//deletion modal  will disaapear .
     }
   };
   function handleTaskchange(e){
@@ -110,6 +127,17 @@ export const Homepage = function () {
         </button>
       </div>
       <ul className="mt-4">
+      {/* for  a in todos 
+      do something  ntil  some condition is met  */}
+      {/* {a,b,c,d}
+      a b c d */}
+
+      {/* {
+       0 activity one ---name--id--dueDate--react
+        1  activity two----name---id--duedate
+        2 activity two----name---id--duedate
+    //   } */}
+    // mapping you are taking all elements in an array  one by one just like using  the lopps like the for loop
         {todos.map((todo, index) => (
           <li key={todo.id} className="border p-2 rounded mt-2 flex justify-between items-center">
             <span>
